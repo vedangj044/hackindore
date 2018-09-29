371ece31f4d2c8365ac4950b479413d9e17706c8
vedangj044 <43697446+vedangj044@users.noreply.github.com>
2018-09-29 15:08:03 +0530

    Add files via upload

---
+++ rep.py
@@ -0,0 +1,17 @@
+
+
+#s =[('SEV TAMATAR',), ('MOONG DAL HALWA',), ('KADAHI PANEER',), ('PANEER TIKKA MASALA',), ('DAL TADKA',), ('PLAIN PARATHA',), ('LACHCHA PARATHA',), ('TAWA ROTI',), ('MIX VEG',), ('PINDI CHOLE',), ('JEERA RICE',), ('ALOO KI SABZI',), ('BUTTER KHICHADI',), ('BIRYANI',), ('IDLI SAMBHAR',), ('MASALA DOSA',), ('POHA',), ('BUTTER SANDWICH',), ('AALOO PARATHA ',), ('FRIED IDLI',), ('GULAB JAMUN',), ('RASGULLA',), ('FRUIT CUSTARD',), ('AAM RAS',)]
+class rep():
+
+    def __init__(self, s):
+        self.s = s
+        populate(self.s)
+
+    def populate(s):
+        l=[]
+        for ei in s:
+            e = str(ei)
+            new = e.replace("(\'", "")
+            new = new.replace("\',)", "")
+            l.append(new)
+        return print(l)
